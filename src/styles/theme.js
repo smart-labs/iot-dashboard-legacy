529e5e85b291aff6d4e83650d2360e4a3880f42d
Joedyson Bezerra <joedysonbezerra@gmail.com>
2019-05-29 03:11:41 -0300

    Adicionando mais cores ao tema

---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+const theme = [
+  {
+    colorBackground: "rgba(230, 67, 86, 0.1)",
+    colorShadow: "rgba(230, 67, 86, 0.61)",
+    primaryColor: "rgba(230, 67, 86, 1)"
+  },
+  {
+    colorBackground: "rgba(33, 217, 154, 0.1)",
+    colorShadow: "rgba(33, 217, 154, 0.61)",
+    primaryColor: "rgba(33, 217, 154, 1)"
+  },
+  {
+    colorBackground: "rgba(255, 205, 80, 0.1)",
+    colorShadow: "rgba(255, 205, 80, 0.61)",
+    primaryColor: "rgba(255, 205, 80, 1)"
+  },
+  {
+    colorBackground: "rgba(0, 204, 255, 0.1)",
+    colorShadow: "rgba(0, 204, 255, 0.61)",
+    primaryColor: "rgba(0, 204, 255, 1)"
+  },
+  {
+    colorBackground: "rgba(176,192, 202, 0.1)",
+    colorShadow: "rgba(176,192, 202, 0.61)",
+    primaryColor: "rgba(176,192, 202, 1)"
+  },
+  {
+    colorBackground: "rgba(167,0, 253, 0.1)",
+    colorShadow: "rgba(167,0, 253, 0.61)",
+    primaryColor: "rgba(167,0, 253, 1)"
+  }
+];
+
+export default theme;
